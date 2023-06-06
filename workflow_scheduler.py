4c1a0e8abaa22d5db97cc69c8885dc40bb5594ce
lyfjourney <joshua.farara@gmail.com>
2023-06-06 10:04:58 -0400

    Changes to be committed: 	modified:   workflow_scheduler.py
    Changes not staged for commit:
    	modified:   venv (untracked content)

--- workflow_scheduler.py
+++ workflow_scheduler.py
@@ -1,17 +1,17 @@
-# from chromeTabs import all
-import chromeTabs 
-import schedule
-import time
-import subprocess
+# # from chromeTabs import all
+# import chromeTabs 
+# import schedule
+# import time
+# import subprocess
 
-def run_script(script_path):
-    subprocess.run(['python', script_path])  # Replace 'python' with the appropriate command if needed
+# def run_script(script_path):
+#     subprocess.run(['python', script_path])  # Replace 'python' with the appropriate command if needed
 
-# Define the schedule for each script
-schedule.every().day.at("08:00").do(run_script, 'script1.py')
-schedule.every().day.at("12:30").do(run_script, 'script2.py')
-schedule.every().day.at("18:45").do(run_script, 'script3.py')
+# # Define the schedule for each script
+# schedule.every().day.at("08:00").do(run_script, 'script1.py')
+# schedule.every().day.at("12:30").do(run_script, 'script2.py')
+# schedule.every().day.at("18:45").do(run_script, 'script3.py')
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
