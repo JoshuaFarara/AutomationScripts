916922c565db6a6584ff3e0c672ad6728652e723
lyfjourney <joshua.farara@gmail.com>
2023-11-22 23:19:12 -0500

    Changes to be committed: 	modified:   Workflows/workflows.py

--- Workflows/workflows.py
+++ Workflows/workflows.py
@@ -4,29 +4,50 @@ import time
 
 class Workflow:
 
-    def __init__(self, workflow_name):
-        self.workflow_name = workflow_name
+    def __init__(self):
+        self.workflow_name = None
         self.website_urls = []
 
-    def build_workflow(self):
-        self.website_urls = self.add_url_to_workflow
+    def set_workflow_name(self, name):
+        self.workflow_name = name
+
     
-    def add_url_to_workflow(workflow):
-        url_to_add = input("Enter a new url...")
-        return url_to_add
+    def add_url_to_workflow(self):
+        if not self.workflow_name:
+            print("Workflow name not set. Please set the name first.")
+            return
+        
+        url_to_add = input("Enter a new URL for {self.workflow_name}: ")
+        self.website_urls.append(url_to_add)
+        print(f"Added {url_to_add} to {self.workflow_name} workflow.")
     
     
-    def open_workflow1():
+    def open_workflow(self):
+        if not self.workflow_name:
+            print("Workflow name not set. Please set the name first.")
+            return
+        
+        if not self.website_urls:
+            print(f"No URLs added to {self.workflow_name} workflow yet.")
+            return
+        
         website_urls = ['https://www.geeksforgeeks.org/100-days-of-code-a-complete-guide-for-beginners-and-experienced/', 'https://www.geeksforgeeks.org/python-programming-language/',
                         'https://www.geeksforgeeks.org/introduction-to-python/', 'https://www.geeksforgeeks.org/python-programming-examples/']
-        open_in_chrome(website_urls)
-        print("Workflow1, loaded successfully.")
-
-    def open_in_chrome(urls):
-    # Path to your Chrome installation
+        
+        # Path to your Chrome installation
         chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-    # Open a new Chrome window with the group of tabs
-        subprocess.Popen([chrome_path, '--new-window'] + urls)
+        # Open a new Chrome window with the group of tabs
+        subprocess.Popen([chrome_path, '--new-window'] + self.website_urls)
+        print(f"Opened {self.workflow_name} workflow in Chrome successfully.")
+  
+    # def open_in_chrome(urls):
+    #     # Path to your Chrome installation
+    #     chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+    #     # Open a new Chrome window with the group of tabs
+    #     subprocess.Popen([chrome_path, '--new-window'] + urls)
 
-workflow=Workflow('ProjectTest')
-workflow.build_workflow()
+
+
+# Now we need to create a workflow process. 
+def build_workflow():
+    pass
